6ca425408a2384f3af534ee372d3e183f2d2eb07
offroadgamer <olafkri05@gmail.com>
2023-05-26 10:22:33 +0200

    Laget Navbar
    Jeg har laget en navbar og satt opp all routing-en slik at det nå er enkelt å navigere seg rundt på nettsiden.

---
+++ Valheim_website/src/routes/+layout.svelte
@@ -0,0 +1,51 @@
+<script>
+
+</script>
+
+<div class="navbar-box">
+	<div class="logo-box">
+		<a href="./"><img src="./src/lib/images/valheim_logo.png" alt="valheim logo"></a>
+	</div>
+	<div class="links-box">
+		<a class="navbar-link-item" href="./BUILDS">BUILDS</a>
+		<a class="navbar-link-item" href="./ENEMIES">ENEMIES</a>
+		<a class="navbar-link-item" href="./ABOUT">ABOUT</a>
+	</div>
+</div>
+
+<style>
+	.navbar-box {
+		background-color: #4E4E4E;
+		min-height: 100px;
+		margin: 0px;
+		padding: 10px;
+		width: 100%;
+		display: flex;
+		flex-direction: row;
+	}
+
+	.logo-box {
+		width: 40%;
+	}
+
+	img {
+		width: 220px;
+	}
+
+	.links-box {
+		width: 60%;
+		display: flex;
+		flex-direction: row;
+		justify-content: space-evenly;
+		align-items: center;
+		margin-right: 60px;
+	}
+
+	.navbar-link-item {
+		text-decoration: none;
+		color: orange;	
+		font-size: 25px;
+	}
+</style>
+
+<slot />
---
+++ Valheim_website/src/routes/+page.svelte
@@ -0,0 +1,30 @@
+<script>
+
+</script>
+
+<body>
+	<div class="main">
+		<div class="front-img-box">
+			<img class="front-img" src="./lib/images" alt="front_image">
+		</div>
+		<h1>VALHEIM</h1>
+
+	</div>
+</body>
+
+<style>
+	body {
+		margin: 0px;
+		font-family: Arial;
+	}
+
+	.main {
+		margin-top: 50px;
+		text-align: center;
+		color: orange;
+	}
+
+	.front-img-box {
+		
+	}
+</style>
